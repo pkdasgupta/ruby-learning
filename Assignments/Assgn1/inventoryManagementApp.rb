066a95e74dd0b83f56a867bba0bd3767fc855fd1
Prashant Dasgupta <prashant.dasgupta@yahoo.com>
2016-06-03 08:06:59 +0530

    Adding assignment Problems

--- Assignments/Assgn1/inventoryManagementApp.rb
+++ Assignments/Assgn1/inventoryManagementApp.rb
@@ -19,7 +19,7 @@
 # -------------------------------------------------------------------------------------------
 
 BEGIN {
-    puts "\n\n **** Inventory Manager **** \n"
+    puts "\n\n ***************** Inventory Manager ****************** \n"
 }
 
 END {
@@ -49,7 +49,7 @@ class Product
         p.name = gets.chomp
 
         print "Enter per unit Price of #{p.name} : "
-        p.price = gets.chomp
+        p.price = gets.to_i
 
         print "Enter initial Number of units of #{p.name} to Add : "
         p.units = gets.to_i
@@ -60,7 +60,14 @@ class Product
         print "Enter Maximum Stock units for #{p.name} : "
         p.max_stock = gets.to_i
 
-        @@products << p
+        if (p.min_stock < p.units) and (p.units < p.max_stock)
+            @@products << p
+            puts "\n Cool. Product details have been saved Successfully.\n\n"
+            p.getProdbyName(p.name)
+        else
+            puts "\n Oops! Looks like initial no. of Units dont fall in the range of Min and Max Stocks."
+            puts " Please give it a retry with Proper entries."
+        end
     end
 
     #Method to display the Registered Products
@@ -80,28 +87,33 @@ class Product
         if opn == "add"
             print "Please provide number of Units to be added to Inventory : "
             units = gets.to_i
-            if prod.max_stock < (prod.units + units)
+            post_add_units = prod.units + units
+            puts "post_add_units : #{post_add_units}, prod_max_stock : #{prod.max_stock}"
+
+            if (prod.max_stock < post_add_units)
                 prod.units += units
             else 
                 puts "Oops! Maximum stock limit is getting exceeded. We can not accomodate #{units}-more units! :("
             end
 
-            if prod.max_stock - prod.units < 10
-                puts "\n ***** Inventory Alert ***** \n We can only #{prod.max_stock - prod.units} more Units. \n Please do the needful to release some Inventory."
+            if prod.max_stock - prod.units <= 2
+                puts "\n ***** Inventory Alert ***** \n We can only accomodate #{prod.max_stock - prod.units} more Units. \n Please do the needful to release some Inventory."
             end
         end
 
         if opn == "remove"
             print "Please provide number of Units to be removed from Inventory : "
             units = gets.to_i
-            if prod.min_stock > (prod.units - units)
+            post_remove_units = prod.units - units
+
+            if prod.min_stock > post_remove_units
                 prod.units -= units
             else 
-                puts "Oops! Minimum stock limit is getting exceeded. We can not release #{units}-more units! :("
+                puts "Oops! Minimum stock limit is getting breached. We can not release #{units}-more units! :("
             end
 
-            if prod.units - prod.min_stock < 10
-                puts "\n ***** Inventory Alert ***** \n We have only #{prod.units} more Units. \n Please do the needful to release some Inventory."
+            if prod.units - prod.min_stock <= 2
+                puts "\n ***** Inventory Alert ***** \n We have only #{prod.units} more Units of #{prod.name} in Stock. \n Please do the needful to procure some Inventory."
             end
         end
     end
@@ -121,6 +133,41 @@ class Product
         puts "--" * 20
     end
 
+    #Method to get Product Details by Name
+    def getProdbyName(name)
+        prod = @@products.find { |p| p.name == name }
+        if prod.nil?
+            puts "Oh! Looks like there are No Products registered with Name : #{name}"
+        else
+            puts "Product Details for : #{name}"
+            puts "\n (*) Product Name : #{prod.name}"
+            puts " (*) Units in Stock : #{prod.units}"
+            puts " (*) Per Unit Price : Rs. #{prod.price} INR"
+            puts " (*) Minimum Stock Units : #{prod.min_stock}"
+            puts " (*) Maximum Stock Units : #{prod.max_stock}"
+        end
+    end
+
+    #Method to get Product Details with Highest/Lowest stocks in Inventory
+    def stockRankings(stat)
+        stock_count = {} #Hash to store Product Name and Stock Units
+        @@products.each { |p| stock_count [p.units] = p.name } 
+
+        if stat == "max"
+            pnames = []
+            stock_count.keys.sort!.last(3).each { |count| pnames << stock_count[count] }
+
+            puts "\n Please find below Product Details for Top-3 Products with maximum units in Stock : "
+            pnames.reverse.each { |pname| getProdbyName(pname) }
+        elsif stat == "min"
+            pnames = [] 
+            stock_count.keys.sort!.first(3).each { |count| pnames << stock_count[count] }
+
+            puts "\n Please find below Product Details for Top-3 Products with minimum units in Stock : "
+            pnames.each { |pname| getProdbyName(pname) }
+        end
+    end
+
     #Method to facilitate User-Actions
     def Product.getUserActions
         loop = "y"
@@ -131,6 +178,7 @@ class Product
             puts "\n 4. View Stock Inventory worth Details."
             puts "\n 5. List Products with Highest stocks in Inventory."
             puts "\n 6. List Products with Lowest stocks in Inventory."
+            puts "\n 7. Make a Product Search."
             
             d = Product.new  #Dummy Product Object to facilitate method Calls
 
@@ -146,11 +194,15 @@ class Product
                 d.modInventory("remove")
             when 4
                 d.worthInventory
-
             when 5
-
+                d.stockRankings("max")
             when 6
+                d.stockRankings("min")
+            when 7
+                print "Please enter the Product Name to Search : "
+                pname = gets.chomp
 
+                d.getProdbyName(pname)
             else 
                 puts "\n Aah.. We don't have that Option yet !"
             end
@@ -161,6 +213,8 @@ class Product
     end
 
 end
+
+#%x("clear") -- To clear/reset the screen 
 
 # Method Call to start App : class method (Called without instanising the Product class)
 Product.getUserActions

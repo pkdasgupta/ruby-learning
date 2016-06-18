2e549c8625ac7f0fc6ec6f2bba300a81b1076dcd
Prashant Dasgupta <prashant.dasgupta@yahoo.com>
2016-06-18 12:08:10 +0530

    Adding More features

---
+++ rails-learning/may-rails/tasksbox/app/controllers/clients_controller.rb
@@ -0,0 +1,26 @@
+class ClientsController < ApplicationController
+    
+    def index
+        @clients = Client.all
+    end
+
+    def new
+        @client = Client.new
+    end
+
+    def create
+        @client = Client.new(client_params)
+        if @client.save
+            redirect_to clients_path, notice: "Cool..Client Successfully Registered ! :)"
+        else
+            render action: "new"
+        end
+    end
+
+    private
+    #To restrict Mass Assignment
+    def client_params
+        params[:client].permit(:name, :company, :email, :mobile)
+    end
+
+end
--- rails-learning/may-rails/tasksbox/config/routes.rb
+++ rails-learning/may-rails/tasksbox/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :clients
+
   get 'garage/clients'
 
   get 'garage/projects'
